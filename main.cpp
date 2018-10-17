a2934f2d394945b16b7da47be43da027ebb4306f
rex <569750687@qq.com>
2018-10-17 16:51:50 +0800

    打印数据

--- main.cpp
+++ main.cpp
@@ -20,7 +20,10 @@
 #include <linux/if_packet.h>
 #include <netinet/if_ether.h>
 #include <iostream>
-#include "OrderBook.h"
+#include <fstream>
+#include "OrderbookData.h"
+#include "TradeData.h"
+#include "ReferenceData.h"
 
 #define BUFF_SIZE 2048
 static const char *g_szIfName = "ens3f1np1"; // 网卡接口
@@ -38,24 +41,16 @@ typedef struct XdpMessageHeader
 {
     unsigned short mMsgSize;
     unsigned short mMsgType;
-} MessageHeader; 
-
-    // 增加/修改订单
-    //     typedef struct XdpAddModOrder
-    // {
-    //     unsigned int mOrderbookID;
-    //     unsigned long long mOrderID;
-    //     int mPrice;
-    //     unsigned int mQuantity;
-    //     unsigned char mSide;
-    //     unsigned char mLotType;
-    //     unsigned short mOrderType;
-    //     unsigned int mOrderbookPosition;
-    // } AddModOrder;
- 
+} MessageHeader;
 
 void ProcessMessageHeader(char *buf, int msgCount);
 void AddOrder(char *buf, uint16_t offset, uint16_t len);
+void Trade(char *buf, uint16_t offset, uint16_t len);
+void CommodityDefinition(char *buf, uint16_t offset, uint16_t len);
+void SeriesDefinitionExtended(char *buf, uint16_t offset, uint16_t len);
+void SeriesDefinitionBase(char *buf, uint16_t offset, uint16_t len);
+void ClassDefinition(char *buf, uint16_t offset, uint16_t len);
+std::string trim(std::string s);
 
 int main(int argc, char *argv[])
 {
@@ -68,7 +63,6 @@ int main(int argc, char *argv[])
     struct ethhdr *eth;
     struct iphdr *iph;
     struct udphdr *udph;
-    printf("%d,%d,%d,%d,%d\n", sizeof(unsigned short), sizeof(unsigned char), sizeof(unsigned int), sizeof(unsigned long), sizeof(unsigned long long));
     sockfd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_IP));
     if (sockfd == -1)
     {
@@ -81,7 +75,7 @@ int main(int argc, char *argv[])
         perror("ioctl()");
         close(sockfd);
         return -1;
-    } 
+    }
     memset(&sll, 0, sizeof(sll));
     sll.sll_family = PF_PACKET;
     sll.sll_ifindex = ifr.ifr_ifindex;
@@ -92,20 +86,20 @@ int main(int argc, char *argv[])
         close(sockfd);
         return -1;
     }
-    
+
     int i = 0;
     while (1)
     {
-        if (i++ == 10)
-            break;
+        // if (i++ == 10)
+        //     break;
         n = recvfrom(sockfd, buf, sizeof(buf), 0, NULL, NULL);
         if (n == -1)
         {
             perror("recvfrom()");
             break;
-        } 
-        eth = (struct ethhdr *)buf; 
-        iph = (struct iphdr *)(buf + sizeof(struct ethhdr)); 
+        }
+        eth = (struct ethhdr *)buf;
+        iph = (struct iphdr *)(buf + sizeof(struct ethhdr));
         if (iph->protocol == IPPROTO_UDP)
         {
             udph = (struct udphdr *)(buf + sizeof(struct ethhdr) + sizeof(struct iphdr));
@@ -116,28 +110,31 @@ int main(int argc, char *argv[])
             int port = ntohs(udph->dest);
             if (hdr->mMsgCount > 0)
             {
-                if (strcmp(ip, "239.1.127.130") == 0 && port == 51001)
+                if (strcmp(ip, "239.1.127.153") == 0 && port == 51003)
                 {
-                    printf("\n==================================================channel id:121 239.1.127.130:51001======================================================\n");
-                    printf("PktSize:%hu,", hdr->mPktSize);
-                    printf("MsgCount:%hu,", hdr->mMsgCount);
-                    printf("SeqNum:%lu\n", hdr->mSeqNum);
-                    ProcessMessageHeader(msgPtr, hdr->mMsgCount);
-                }
-                if (strcmp(ip, "239.1.127.130") == 0 && port == 51002)
-                {
-                    printf("\n==================================================channel id:221 239.1.127.130:51002======================================================\n");
-                    printf("PktSize:%hu,", hdr->mPktSize);
-                    printf("MsgCount:%hu,", hdr->mMsgCount);
-                    printf("SeqNum:%lu\n", hdr->mSeqNum);
+                    // printf("\n==================================================channel id:121 239.1.127.130:51001======================================================\n");
+                    // printf("PktSize:%hu,", hdr->mPktSize);
+                    // printf("MsgCount:%hu,", hdr->mMsgCount);
+                    // printf("SeqNum:%lu\n", hdr->mSeqNum);
                     ProcessMessageHeader(msgPtr, hdr->mMsgCount);
                 }
             }
-
         }
     }
     close(sockfd);
     return 0;
+}
+
+std::string trim(std::string s)
+{
+
+    if (!s.empty())
+    {
+        s.erase(0, s.find_first_not_of(" "));
+        s.erase(s.find_last_not_of(" ") + 1);
+    }
+
+    return s;
 }
 
 void ProcessMessageHeader(char *buf, int msgCount)
@@ -147,22 +144,43 @@ void ProcessMessageHeader(char *buf, int msgCount)
     while (n < msgCount)
     {
         MessageHeader *msghdr = (MessageHeader *)buf;
-        printf("msg %d: msgsize=%d, msgtype=%d\n", n, msghdr->mMsgSize, msghdr->mMsgType);
-        if (msghdr->mMsgType == ADDORDER)
+        // printf("msg %d: msgsize=%d, msgtype=%d\n", n, msghdr->mMsgSize, msghdr->mMsgType);
+        // if (msghdr->mMsgType == ADDORDER)
+        // {
+        //     AddOrder(buf, 4, msghdr->mMsgSize);
+        // }
+        // if (msghdr->mMsgType == TRADE)
+        // {
+        //     Trade(buf, 4, msghdr->mMsgSize);
+        // }
+
+        // if (msghdr->mMsgType == COMMODITYDEFINITION)
+        // {
+        //     CommodityDefinition(buf, 4, msghdr->mMsgSize);
+        // }
+        // if (msghdr->mMsgType == CLASSDEFINITION)
+        // {
+        //     ClassDefinition(buf, 4, msghdr->mMsgSize);
+        // }
+        // if (msghdr->mMsgType == SERIESDEFINITIONBASE)
+        // {
+        //     SeriesDefinitionBase(buf, 4, msghdr->mMsgSize);
+        // }
+        if (msghdr->mMsgType == SERIESDEFINITIONEXTENDED)
         {
-            AddOrder(buf, 4, msghdr->mMsgSize);
+            SeriesDefinitionExtended(buf, 4, msghdr->mMsgSize);
         }
         n++;
         buf = buf + msghdr->mMsgSize;
         size += msghdr->mMsgSize;
     }
-    printf("total size=%d\n", size + 16);
+    // printf("total size=%d\n", size + 16);
 }
 
 void AddOrder(char *buf, uint16_t offset, uint16_t len)
 {
     XdpAddModOrder addOrder(buf, len, offset);
-    printf("OrderbookID:%u\n", addOrder.orderBookId());
+    printf("OrderbookID:%u\n", addOrder.orderbookId());
     printf("OrderID:%llu\n", addOrder.orderId());
     printf("price:%d\n", addOrder.price());
     printf("quantity:%u\n", addOrder.quantity());
@@ -170,5 +188,141 @@ void AddOrder(char *buf, uint16_t offset, uint16_t len)
     printf("lotType:%d\n", addOrder.lotType());
     printf("OrderType:%d\n", addOrder.orderType());
     printf("orderBookPosition:%u\n\n", addOrder.orderbookPosition());
+}
 
+void Trade(char *buf, uint16_t offset, uint16_t len)
+{
+    XdpTrade trade(buf, len, offset);
+
+    printf("tradeTime:%u\n", trade.tradeTime());
+    printf("orderbookId:%u\n", trade.orderbookId());
+    printf("orderId:%llu\n", trade.orderId());
+    printf("tradeId:%llu\n", trade.tradeId());
+    printf("comboGroupId:%u\n", trade.comboGroupId());
+    printf("price:%d\n", trade.price());
+    printf("quantity:%u\n", trade.quantity());
+    printf("side:%hhu\n", trade.side());
+    printf("dealType:%hhu\n", trade.dealType());
+    printf("tradeCondition:%hu\n", trade.tradeCondition());
+    printf("dealInfo:%hu\n\n", trade.dealInfo());
+}
+
+void CommodityDefinition(char *buf, uint16_t offset, uint16_t len)
+{
+    XdpCommodityDefinition commodity(buf, len, offset);
+    // std::ofstream outfile("commodity.txt", std::ios::app);
+    // outfile << "commodityCode:" << commodity.commodityCode() << std::endl;
+    // outfile << "commodityId:" << commodity.commodityId() << std::endl;
+    // outfile << "underlyingCode:" << commodity.underlyingCode() << std::endl;
+    // outfile << "commodityName:" << commodity.commodityName() << std::endl;
+    // outfile << "isinCode:" << commodity.isinCode() << std::endl;
+    // outfile << "baseCurrency:" << commodity.baseCurrency() << std::endl;
+    // outfile << "decimalUnderlyingPrice:" << commodity.decimalUnderlyingPrice() << std::endl;
+    // outfile << "underlyingPriceUnit:" << +commodity.underlyingPriceUnit() << std::endl;
+    // outfile << "nominalValue:" << commodity.nominalValue() << std::endl;
+    // outfile << "underlyingType:" << +commodity.underlyingType() << std::endl;
+    // outfile << "effectiveTomorrow:" << +commodity.effectiveTomorrow() << std::endl;
+    // outfile << "===============================================================" << std::endl;
+    // outfile.close();
+    if (trim(commodity.commodityName()) == "HANG SENG INDEX")
+    {
+        std::cout << "commodityCode:" << commodity.commodityCode() << std::endl;
+        std::cout << "commodityId:" << commodity.commodityId() << std::endl;
+        std::cout << "underlyingCode:" << commodity.underlyingCode() << std::endl;
+        std::cout << "commodityName:" << commodity.commodityName() << std::endl;
+        std::cout << "isinCode:" << commodity.isinCode() << std::endl;
+        std::cout << "baseCurrency:" << commodity.baseCurrency() << std::endl;
+        std::cout << "decimalUnderlyingPrice:" << commodity.decimalUnderlyingPrice() << std::endl;
+        std::cout << "underlyingPriceUnit:" << +commodity.underlyingPriceUnit() << std::endl;
+        std::cout << "nominalValue:" << commodity.nominalValue() << std::endl;
+        std::cout << "underlyingType:" << +commodity.underlyingType() << std::endl;
+        std::cout << "effectiveTomorrow:" << +commodity.effectiveTomorrow() << std::endl;
+        std::cout << "===============================================================" << std::endl;
+    }
+}
+
+void SeriesDefinitionBase(char *buf, uint16_t offset, uint16_t len)
+{
+    XdpSeriesDefinitionBase sdb(buf, len, offset);
+    std::cout << "orderbookId:" << sdb.orderbookId() << std::endl;
+    std::cout << "symbol:" << sdb.symbol() << std::endl;
+    std::cout << "financialProduct" << +sdb.financialProduct() << std::endl;
+    std::cout << "numberOfDecimalsPrice:" << sdb.numberOfDecimalsPrice() << std::endl;
+    std::cout << "numberOfLegs" << +sdb.numberOfLegs() << std::endl;
+    std::cout << "expirationDate:" << sdb.expirationDate() << std::endl;
+    std::cout << "putOrCall:" << +sdb.putOrCall() << std::endl;
+    std::cout << "===============================================================" << std::endl;
+}
+
+void SeriesDefinitionExtended(char *buf, uint16_t offset, uint16_t len)
+{
+    XdpSeriesDefinitionExtented sde(buf, len, offset);
+    std::ofstream outfile("extended.txt", std::ios::app);
+    if ((sde.commodityCode() == 4002  || sde.commodityCode() == 4010) && sde.instrumentGroup() == 4)
+    {
+        outfile << "orderbookId" << sde.orderbookId() << std::endl;
+        outfile << "symbol:" << sde.symbol() << std::endl;
+        outfile << "country" << +sde.country() << std::endl;
+        outfile << "market:" << +sde.market() << std::endl;
+        outfile << "instrumentGroup:" << +sde.instrumentGroup() << std::endl;
+        outfile << "modifier:" << +sde.modifier() << std::endl;
+        outfile << "commodityCode:" << sde.commodityCode() << std::endl;
+        outfile << "expirationDate:" << sde.expirationDate() << std::endl;
+        outfile << "strikePrice:" << sde.strikePrice() << std::endl;
+        outfile << "contractSize:" << +sde.contractSize() << std::endl;
+        outfile << "isinCode:" << sde.isinCode() << std::endl;
+        outfile << "seriesStatus:" << +sde.seriesStatus() << std::endl;
+        outfile << "effectiveTomorrow:" << +sde.effectiveTomorrow() << std::endl;
+        outfile << "priceQuotationFactor:" << sde.priceQuotationFactor() << std::endl;
+        outfile << "effectiveExpDate:" << sde.effectiveExpDate() << std::endl;
+        outfile << "dateTimeLastTrading:" << sde.dateTimeLastTrading() << std::endl;
+        outfile << "===============================================================" << std::endl;
+    }
+    std::cout << "orderbookId" << sde.orderbookId() << std::endl;
+    std::cout << "symbol:" << sde.symbol() << std::endl;
+    std::cout << "country" << +sde.country() << std::endl;
+    std::cout << "market:" << +sde.market() << std::endl;
+    std::cout << "instrumentGroup:" << +sde.instrumentGroup() << std::endl;
+    std::cout << "modifier:" << +sde.modifier() << std::endl;
+    std::cout << "commodityCode:" << sde.commodityCode() << std::endl;
+    std::cout << "expirationDate:" << sde.expirationDate() << std::endl;
+    std::cout << "strikePrice:" << sde.strikePrice() << std::endl;
+    std::cout << "contractSize:" << +sde.contractSize() << std::endl;
+    std::cout << "isinCode:" << sde.isinCode() << std::endl;
+    std::cout << "seriesStatus:" << +sde.seriesStatus() << std::endl;
+    std::cout << "effectiveTomorrow:" << +sde.effectiveTomorrow() << std::endl;
+    std::cout << "priceQuotationFactor:" << sde.priceQuotationFactor() << std::endl;
+    std::cout << "effectiveExpDate:" << sde.effectiveExpDate() << std::endl;
+    std::cout << "dateTimeLastTrading:" << sde.dateTimeLastTrading() << std::endl;
+    std::cout << "===============================================================" << std::endl;
+}
+
+void ClassDefinition(char *buf, uint16_t offset, uint16_t len)
+{
+    XdpClassDefinition cd(buf, len, offset);
+
+    if (cd.commodityCode() == 5306)
+    {
+        printf("country:%hhu\n", cd.country());
+        printf("market:%hhu\n", cd.market());
+        printf("instrumentGroup:%hhu\n", cd.instrumentGroup());
+        printf("modifier:%hhu\n", cd.modifier());
+        printf("commodityCode:%hu\n", cd.commodityCode());
+        printf("expirationDate:%d\n", cd.priceQuotationFactor());
+        printf("strikePrice:%u\n", cd.contractSize());
+        printf("contractSize:%hu\n", cd.decimalInStrikePrice());
+        printf("decimalInContractSize:%hu\n", cd.decimalInContractSize());
+        printf("decimalInPremium:%hu\n", cd.decimalInPremium());
+        printf("rnkingType:%hu\n", cd.rnkingType());
+        printf("tradable:%hhu\n", cd.tradable());
+        printf("premiumUnit4Price:%hhu\n", cd.premiumUnit4Price());
+        std::cout << "baseCurrency:" << cd.baseCurrency() << std::endl;
+        std::cout << "instrumentClassID:" << cd.instrumentClassID() << std::endl;
+        std::cout << "instrumentClassName:" << cd.instrumentClassName() << std::endl;
+        std::cout << "isFractions:" << cd.isFractions() << std::endl;
+        std::cout << "settlementCurrencyID:" << cd.settlementCurrencyID() << std::endl;
+        printf("effectiveTomorrow:%hhu\n", cd.effectiveTomorrow());
+        printf("tickStepSize:%d\n\n", cd.tickStepSize());
+        std::cout << "===============================================================" << std::endl;
+    }
 }
