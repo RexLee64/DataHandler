26e26458c1b70c2396af8a09eb550d7b8c69c1e6
rex <569750687@qq.com>
2018-10-19 13:39:02 +0800

    修正订单

--- OrderbookData.h
+++ OrderbookData.h
@@ -2,15 +2,12 @@
 #define ORDERBOOKDATA_H
 #include "BaseData.h"
 
-class XdpAddModOrder : public XdpBaseData
+class XdpAddOrder : public XdpBaseData
 {
   public:
-    XdpAddModOrder() {}
-    XdpAddModOrder(char *buffer, const uint16_t length, const uint16_t offset = 0) : XdpBaseData(buffer, length, offset) {}
-    // XdpAddModOrder(char *buffer, const uint16_t length, const uint16_t offset)
-    // {
-    //     reset(buffer, length, 0);
-    // }
+    XdpAddOrder() {}
+    XdpAddOrder(char *buffer, const uint16_t length, const uint16_t offset = 0) : XdpBaseData(buffer, length, offset) {}
+
     uint32_t orderbookId()
     {
         uint32_t val;
@@ -45,6 +42,56 @@ class XdpAddModOrder : public XdpBaseData
     {
         uint8_t val;
         memcpy(&val, mBuffer + mOffset + 21, sizeof(uint8_t));
+        return val;
+    }
+    uint16_t orderType()
+    {
+        uint16_t val;
+        memcpy(&val, mBuffer + mOffset + 22, sizeof(uint16_t));
+        return val;
+    }
+    uint32_t orderbookPosition()
+    {
+        uint32_t val;
+        memcpy(&val, mBuffer + mOffset + 24, sizeof(uint32_t));
+        return val;
+    }
+};
+
+class XdpModifyOrder : public XdpBaseData
+{
+  public:
+    XdpModifyOrder() {}
+    XdpModifyOrder(char *buffer, const uint16_t length, const uint16_t offset = 0) : XdpBaseData(buffer, length, offset) {}
+
+    uint32_t orderbookId()
+    {
+        uint32_t val;
+        memcpy(&val, mBuffer + mOffset, sizeof(uint32_t));
+        return val;
+    }
+    uint64_t orderId()
+    {
+        uint64_t val;
+        memcpy(&val, mBuffer + mOffset + 4, sizeof(uint64_t));
+        return val;
+    }
+    int32_t price()
+    {
+        int32_t val;
+        memcpy(&val, mBuffer + mOffset + 12, sizeof(int32_t));
+        return val;
+    }
+    uint32_t quantity()
+    {
+        uint32_t val;
+        memcpy(&val, mBuffer + mOffset + 16, sizeof(uint32_t));
+        return val;
+    }
+    uint8_t side()
+    {
+        uint8_t val;
+        memcpy(&val, mBuffer + mOffset + 20, sizeof(uint8_t));
         return val;
     }
     uint16_t orderType()
